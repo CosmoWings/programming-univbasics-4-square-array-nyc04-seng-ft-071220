c82231cdeeeb31b29034893b0a2b83761cfb65c8
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-17 21:37:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   squared = []
   array.length.times do |arr_element|
-    squared.push(array[arr_element] * array[arr_element])
+    squared.push(array[arr_element] **2)
   end
   squared
 end
